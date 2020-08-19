c90299923f6d936436a2b070f70792ceca01766f
manonaskateb <manonaskate@gmail.com>
2020-08-19 10:40:56 -0700

    Cleaned up java code for exponentials

--- JAVA/SolveForExp/Exponentials.java
+++ JAVA/SolveForExp/Exponentials.java
@@ -1,6 +1,4 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
+
 import javax.swing.JOptionPane;
 
 class Exponentials{
@@ -12,7 +10,6 @@ class Exponentials{
         int result = Integer.parseInt(resultNumber);
         int output_factor;
 
-
         output_factor = solveForExp(base, result);
 
         JOptionPane.showMessageDialog(null, "the exponent is: " + output_factor , "output_factor", JOptionPane.PLAIN_MESSAGE );
@@ -20,7 +17,6 @@ class Exponentials{
     }
     
     public static int solveForExp(int n, int m){
-        int factor;
         if (m==1) {return 0;}
         else {
             return (int)(Math.log(m)/Math.log(n));
