b32088aa23ce62cab8c694c82b7b13fd3416d1c7
Manon Lefebvre <manonalefebvre@gmail.com>
2020-08-18 17:48:15 -0700

    finding exponent solveForExp and main() created

--- JAVA/SolveForExp/solveForExp.java
+++ JAVA/SolveForExp/solveForExp.java
@@ -1,1 +1,12 @@
 import java.io.ioexception;
+import java.util.*;
+import java.math.log;
+
+public static int solveForExp(int n, int m){
+    int factor;
+    if (m==1) {factor = 0;}
+    else {
+        factor = (int)(Math.log(n)/Math.log(m));
+    }
+    return factor;
+}
