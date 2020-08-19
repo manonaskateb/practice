fe39dd6a0d0dd0f0e76b67dd763a79dda99fa059
manonaskateb <manonaskate@gmail.com>
2020-08-18 19:16:21 -0700

    Dont with exponent calculator! Made a gui for it

--- JAVA/SolveForExp/Exponentials.java
+++ JAVA/SolveForExp/Exponentials.java
@@ -1,16 +1,21 @@
 import java.util.*;
 import java.io.*;
 import java.math.*;
+import javax.swing.JOptionPane;
 
 class Exponentials{
     public static void main(String[] args){
-        int input_int = Integer.parseInt(args[0]);
-        int input_mult =  Integer.parseInt(args[1]);
+        String baseNumber = JOptionPane.showInputDialog("baseNumber");
+        String resultNumber =  JOptionPane.showInputDialog("resultNumber");
+
+        int base = Integer.parseInt(baseNumber);
+        int result = Integer.parseInt(resultNumber);
         int output_factor;
 
-        output_factor = solveForExp(input_int, input_mult);
 
-        System.out.print(input_int + " to the power of " + output_factor + " is equal to " + input_mult + ".\n");
+        output_factor = solveForExp(base, result);
+
+        JOptionPane.showMessageDialog(null, "the exponent is: " + output_factor , "output_factor", JOptionPane.PLAIN_MESSAGE );
         return;
     }
     
