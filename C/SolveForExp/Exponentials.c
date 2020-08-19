c287334c3af2f1cfceb60917a0be19ce53b512cd
manonaskateb <manonaskate@gmail.com>
2020-08-19 14:12:56 -0700

    Somethings wrong with the functions im using or something for logarithms in c

--- C/SolveForExp/Exponentials.c
+++ C/SolveForExp/Exponentials.c
@@ -1,22 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <tgmath.h>
+#include <math.h>
+
+double findExp(int b, int r){
+    return log(r)/log(b);
+}
 
 int main() {
-    double base;
-    printf("Enter the base number:\n");
-    scanf("%d", &base);
+    setbuf(stdout, NULL); // disabling buffer so the printf happens before the scanf (so user knows when and what number to put in)
 
+    printf("Enter the base number:");
+    double base;
+    scanf("%d", &base); 
+
+    printf("Enter the result number:");
     double result;
-    printf("Enter the result number:\n");
     scanf("%d", &result);
 
-    double r_log = log(result);
-    double b_log = log(base);
-
-    double exponent = 0;
-    exponent = r_log/b_log;
-    printf("%d to the power of %d is %d.", base, exponent, result);
+    double exponent = findExp(base, result);
+    printf("%d to the power of %d is %d.\n", base, exponent, result);
 
     return 0;
 }
